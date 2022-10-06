6c3821e7be17e41ca4c043c85a1f8216b0e05b14
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-10-06 09:11:19 +0900

    [add] コマンドライン上で動く環境を追加

--- cui/build.gradle.kts
+++ cui/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.7.10"
+    application
 }
 
 dependencies {
     implementation(project(":domain"))
     implementation(project(":usecase"))
     testImplementation(libs.bundles.test)
+}
+
+application {
+    mainClass.set("MainKt")
 }
